59c5e926fb2fe1d52db10f80b8522bf171dc9896
rodraah <roh_marques12@hotmail.com>
2023-02-09 12:10:25 -0300

    Splited code into 'main.rs' and 'util.rs'

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use gtk4 as gtk;
 use gtk::prelude::*;
 use gtk::gdk;
+use util::*;
 use std::{
     fs::{create_dir_all, read_dir, File}, 
     path::PathBuf,
     io::Read,
     process::Command
 };
+
+mod util;
 
 fn main() {
     let application = gtk::Application::builder()
@@ -176,17 +179,4 @@ fn get_app_path() -> PathBuf {
         });
     }
     apps_path
-}
-
-// Helper
-fn split_string(line: &str, cut: &str, position: usize) -> String {
-    let almost_cut = line.split_once(cut).unwrap_or_default();
-    // Isn't necessary to handle the case where the string isn't cutted
-    let cutted;
-    match position {
-        0 => cutted = almost_cut.0.to_string(),
-        1 => cutted = almost_cut.1.to_string(),
-        _ => panic!(),
-    };
-    cutted
 }
